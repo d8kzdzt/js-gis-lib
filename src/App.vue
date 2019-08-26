9c7714e3341728922c6da925d38f517bbe546ea2
Direction <1429452833@qq.com>
2019-08-26 15:17:10 +0800

    js版本gis库

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-input type="textarea" :rows="10" v-model="srcdata" placeholder="点击生成数据按钮"></el-input>
+    <el-button type="primary" id="genData">生成数据</el-button>
+    <el-input type="textarea" :rows="10" v-model="desdata"></el-input>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: "#app",
+  data() {
+    return {
+      srcdata: "",
+      desdata:""
+    };
   }
+};
+</script>
+
+<style lang="scss" scoped>
+#genData {
+  margin: 20px 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from '@/routers'
+import store from '@/stores'
 
+import Element from 'element-ui'
+
+import '@/styles/index.scss' // global css
+
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
+Vue.use(Element, {
+  size: 'medium'
+})
 new Vue({
   router,
   store,
