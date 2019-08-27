cfb8296cb2d5f282d7e6e9d220447950cdca181b
Direction <1429452833@qq.com>
2019-08-27 16:14:22 +0800

    支持修改数据源，方便测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-input type="textarea" :rows="10" placeholder="点击生成数据按钮" :value="ptArray | tostring" readonly />
+    <el-input type="textarea" :rows="10" placeholder="点击生成数据按钮" v-model="srcData" />
     <div>
       输入指定点P1
       <span>X1:</span><el-input-number v-model="x1" style="width:150px" />
@@ -9,9 +9,9 @@
       <span>X2:</span><el-input-number v-model="x2" style="width:150px" />
       <span>Y2:</span><el-input-number v-model="y2" style="width:150px"/>
       <el-button type="primary" @click="GenData" style="margin-left:20px;" >生成数据</el-button>
-      <el-button type="primary" @click="RunDemo" style="margin-left:20px;" :disabled="ptArray.length == 0">调用线路规划</el-button>
+      <el-button type="primary" @click="RunDemo" style="margin-left:20px;" :disabled="srcData.length == 0">调用线路规划</el-button>
     </div>
-    <el-input type="textarea" :rows="10" :value="desdata | tostring" readonly></el-input>
+    <el-input type="textarea" :rows="10" :value="tostring(desdata)" readonly></el-input>
   </div>
 </template>
 
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       // 输入参数之组成Polyline的点集合
-      ptArray: [],
+      srcData: '',
       desdata: [],
       x1:0,
       y1:0,
@@ -34,28 +34,34 @@ export default {
     };
   },
   filters:{
-    tostring(pts){
-      let res = '';
-      pts.forEach(p => {
-        res += `(${p.x},${p.y})\t`;
-      });
-      return res;
-    }
   },
   methods: {
-    GenData() {
-      this.ptArray=[];
-      GenerateData().forEach(item=>{
-        this.ptArray.push(new Point(item.x,item.y)) ;
+    tostring(pts){
+      var ary = [];
+      pts.forEach(p => {
+        ary.push(`(${p.x},${p.y})`);
       });
+      return ary.join(';');
+    },
+    GenData() {
+      this.srcData = this.tostring(GenerateData());
     },
     RunDemo() {
       // 生成参数,折线pl，p1,p2
-      var pl = new Polyline(this.ptArray);
+      var pl = new Polyline(this.parse2PtArray(this.srcData));
       var p1 = new Point(this.x1,this.y1);
       var p2 = new Point(this.x2,this.y2);
       // 调用并输出
       this.desdata = GetPointsInPolylineBySpecificRange(pl,p1,p2);
+    },
+    // 将字符串解析为点数组
+    parse2PtArray(data){
+      var pts=[];
+      data.split(';').forEach(item=>{
+        var tmp = item.slice(1,item.length-1).split(',');
+        pts.push(new Point(parseFloat(tmp[0]),parseFloat(tmp[1])));
+      });
+      return pts;
     }
   }
 };
